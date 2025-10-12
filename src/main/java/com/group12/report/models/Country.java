5aa1ae1a8d0631339a6b3ea9bfd239a05bf0dd5e
LuMinAung40794408 <40794408@live.napier.ac.uk>
2025-10-13 01:58:15 +0630

    update

--- src/main/java/com/group12/report/models/Country.java
+++ src/main/java/com/group12/report/models/Country.java
@@ -1,14 +1,4 @@
 package com.group12.report.models;
 
-/**
- * Represents a country entity
- */
-public class Country
-{
-    public String code;
-    public String name;
-    public String continent;
-    public String region;
-    public int population;
-    public int capital;
+public class Country {
 }
--- src/main/java/com/group12/report/reports/CountryReport.java
+++ src/main/java/com/group12/report/reports/CountryReport.java
@@ -1,26 +1,6 @@
 package com.group12.report.reports;
 
-import com.group12.report.models.Country;
-import java.util.ArrayList;
-
 public class CountryReport
 {
-    public void displayCountries(ArrayList<Country> countries)
-    {
-        if (countries == null)
-        {
-            System.out.println("No countries to display.");
-            return;
-        }
 
-        System.out.printf("%-6s %-45s %-15s %-20s %-12s%n",
-                "Code", "Name", "Continent", "Region", "Population");
-        System.out.println("-------------------------------------------------------------------------------------");
-
-        for (Country c : countries)
-        {
-            System.out.printf("%-6s %-45s %-15s %-20s %-12d%n",
-                    c.code, c.name, c.continent, c.region, c.population);
-        }
-    }
 }
