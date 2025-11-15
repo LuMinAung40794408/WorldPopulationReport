1be956632108601e08ef466f8a6828a545339d26
LuMinAung40794408 <40794408@live.napier.ac.uk>
2025-11-16 02:54:34 +0630

    Unit check test 4

---
+++ src/test/java/com/group12/testing/AppTest.java
@@ -0,0 +1,22 @@
+package com.group12.testing;
+
+import com.group12.report.App;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+}
